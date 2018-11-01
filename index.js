ba190fb7a72f4b2b6be1247e59932a9f5dd0f37f
Stefan Cardenas <fets809@gmail.com>
2018-11-01 14:50:28 -0500

    Modify how options are defined

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require("http");
 var https = require("https");
 var path = require("path");
 var url = require("url");
+var { URL } = require("url");
 var events = require("events").EventEmitter;
 var util = require("util");
 
@@ -101,13 +102,23 @@ Scraper.prototype.scrape = function(callback){
 		protocol = https;
 	}
 
-	var options = {
-		headers: {
-			'User-Agent': 'javascript'
-		}
+	parsedUrl.headers = {
+		'User-Agent': 'javascript'
 	};
 
-	var request = protocol.request(this.address, options, function(response){
+	// var options = new URL(this.address);
+
+	// options.headers = {
+	// 	'User-Agent': 'javascript'
+	// };
+
+	// var options = {
+	// 	headers: {
+	// 		'User-Agent': 'javascript'
+	// 	}
+	// };
+
+	var request = protocol.request(parsedUrl, function(response){
 
 		if(response.statusCode != 200){
 			console.error("Image scraper(1): web page couldn't be found. (statusCode:" + response.statusCode + ")");
